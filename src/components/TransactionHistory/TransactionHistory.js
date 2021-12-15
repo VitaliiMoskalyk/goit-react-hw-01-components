fd0a6e8517b96608965a1c16d61d2cfdb66c6348
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-12-15 21:37:54 +0200

    third_task

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,5 +1,22 @@
 import propTypes from 'prop-types';
-import { Wrapper, Thead, Article } from './TransactionHistory.styled';
+import {
+  Wrapper,
+  Thead,
+  Article,
+  Row,
+  MainRow,
+} from './TransactionHistory.styled';
+
+const colors = [
+  '#00FF7F',
+  '#FF1493',
+  '#FFD700',
+  '#FF00FF',
+  '#8A2BE2',
+  '#0000CD',
+  '#DAA520',
+  '	#D2691E',
+];
 
 const TableHistory = ({ items }) => {
   return (
@@ -14,11 +31,14 @@ const TableHistory = ({ items }) => {
 
       <tbody>
         {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
-          </tr>
+          <MainRow
+            key={id}
+            id={colors[Math.floor(Math.random() * colors.length)]}
+          >
+            <Row>{type}</Row>
+            <Row>{amount}</Row>
+            <Row>{currency}</Row>
+          </MainRow>
         ))}
       </tbody>
     </Wrapper>
@@ -27,4 +47,13 @@ const TableHistory = ({ items }) => {
 
 export default TableHistory;
 
-TableHistory.propTypes = {};
+TableHistory.propTypes = {
+  items: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      type: propTypes.string.isRequired,
+      amount: propTypes.string.isRequired,
+      currency: propTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
--- src/components/TransactionHistory/TransactionHistory.styled.js
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -5,8 +5,10 @@ export const Wrapper = styled.table`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid tomato;
-  margin-top: 16px;
+  border: 0.1px solid rgb(213, 218, 218);
+  box-shadow: 17px 18px 15px -5px rgba(68, 67, 67, 0.75);
+  font-family: cursive;
+  margin: 16px 0;
 
   width: 250px;
   margin-left: auto;
@@ -19,6 +21,15 @@ export const Thead = styled.thead`
 
 export const Article = styled.th`
   text-align: center;
-  color: green;
   width: 83px;
+`;
+
+export const Row = styled.td`
+  width: 83px;
+  text-align: start;
+  border: 0.1px solid rgb(213, 218, 218);
+`;
+
+export const MainRow = styled.tr`
+  background-color: ${props => props.id}6e;
 `;
